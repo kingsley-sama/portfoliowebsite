27e859963d9ac2a0ac5d49dacf1a188b1220b803
Kingsley Okpo <kngsley2018@gmail.com>
2024-12-23 02:08:24 +0100

    fixes menubar

---
+++ obj_unpack.py
@@ -0,0 +1,13 @@
+obj = {
+    "name": "kingley",
+    "title": "joma",
+    "day": 2,
+    "email":"me@gmail.com"
+}
+
+def print_obj(name, title, day, email):
+    print(f"my name is {name} and my title is {title} i joined on {day}, this is my email {email}")
+
+
+print_obj(**obj)
+print(**obj)
