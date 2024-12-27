3e4be5e75293191ed7a09d2c89128ea6018f68f6
Kingsley Okpo <kngsley2018@gmail.com>
2024-12-28 00:11:50 +0100

    adds vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import './index.css'
 import NeomaLandingPage from './neoma'
 import { Analytics } from '@vercel/analytics/react'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Analytics>
-    <NeomaLandingPage/>
-    </Analytics>
-  </StrictMode>,
+   <NeomaLandingPage/>
+   <Analytics />
+     </StrictMode>,
 )
